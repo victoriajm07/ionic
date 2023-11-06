c17a0df342ee1dfb0dbd8c851a4ec778f9455d18
victoriajm07 <victoria10444@gmail.com>
2023-11-06 22:46:29 +0000

    curso de ionic . Video https://www.youtube.com/watch?v=OYeA21U7g2Q&list=PLsngLoGbAagFEG-jwlpPhGsLzMSQ0tadP&index=14

--- src/app/cities/cities.page.ts
+++ src/app/cities/cities.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
+import { map} from 'rxjs/operators';
+import { AlertController, ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.page.html',
@@ -7,9 +13,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitiesPage implements OnInit {
 
-  constructor() { }
+
+  cities: any = [];
+
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    public toastController: ToastController,
+    public alertController: AlertController) { }
 
   ngOnInit() {
+    this.getCities().subscribe(
+      res=>{
+        console.log("Res", res)
+        this.cities=res;
+      }
+    )
   }
 
+  getCities() {
+    return this.http
+    .get("assets/files/cities.json")
+    .pipe(
+      map((res:any) =>  {
+        return res.data;
+      })
+    )
+  }
+
+  async presentToast1() {
+    const toast = await this.toastController.create({
+      message: 'Ciudad seleccionada',
+      duration: 2000,
+      position: "bottom"
+    })
+    toast.present()
+  }
+
+  async presentAlert1() {
+    const alert = await this.alertController.create({
+      header: "Borrar ciudad",
+      message: "Se ha borrado la ciudad correctamente",
+      buttons: ["OK"]
+    });
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result)
+  }
+
+  async presentAlert2() {
+    const alert = await this.alertController.create({
+      header: "Borrar ciudad",
+      message: "¿Estas seguro?",
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log("No cancel")
+          }
+        },
+        {
+          text: 'Si',
+          handler: () => {
+            console.log("Elminada")
+          }
+        }
+      ]
+    });
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result)
+  }
 }
